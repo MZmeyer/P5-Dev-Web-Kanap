1c3ab655697e8880a87bebdf6b9a6ca32fa97f1d
MZmeyer <mzuckmeyer@live.fr>
2022-11-15 19:31:24 +0100

    Base fonctions ,Produit , anchor id

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,23 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((donnees) => {
-        return Produit(donnees)
-    })
+    .then((donnees) =>  produit(donnees))
+
+function  produit (donnees){
+    let id = donnees[0]._id
+    let anchor = makeA(id)
+    itemChild(anchor)
+}   
+ 
+function makeA(id)    {
+    let anchor = document.createElement("a")
+    anchor.href="./product.html?id="+id
+    return anchor
+}
     
 
+function itemChild(anchor) {
+    let items = document.getElementById("items")
+    if (items!=null){
+    items.appendChild(anchor)
+    }
+}
