b9e0c5cf4fbc27a65c685ca4e8ed5ac6baa4b105
MZmeyer <mzuckmeyer@live.fr>
2022-12-06 17:52:02 +0100

    Ajout loop sur Carts + base création article

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,11 +1,30 @@
 const Cartcontents = localStorage.length;
-const Cart =[];
+const Carts =[];
 
-function RecupStorage(){
+GetStorage()
+for (let Content of Carts){Display(Content)}
+
+
+function GetStorage(){
 for (let i=0; i < Cartcontents; i++){
     const Content = localStorage.getItem(localStorage.key(i));
     const ContentObject = JSON.parse(Content);
-    Cart.push(ContentObject)
+    Carts.push(ContentObject)
     }
 }
-    
+
+function Display(Content){
+    const Article = MakeArticle(Content)
+    ShowArticle(Article)
+}
+function ShowArticle(Article){
+    document.getElementById("cart__items").appendChild(Article)
+
+}
+function MakeArticle(Content){
+    const Article = document.createElement("article")
+    Article.classList.add("cart__item")
+    Article.dataset.id = Content.id
+    Article.dataset.color = Content.color
+    return Article
+}
